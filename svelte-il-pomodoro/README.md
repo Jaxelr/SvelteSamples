4168b0c7e08e270984350ee70b1a20fbb7c9f862
Jaxel Rojas <jaxelr@gmail.com>
2020-01-29 10:18:38 -0400

    feat: added svelte il pomodoro tutorial

---
+++ svelte-il-pomodoro/README.md
@@ -0,0 +1,3 @@
+# Svelte Il Pomodoro
+
+Taken from  [this article](https://www.barbarianmeetscoding.com/blog/2019/12/27/discovering-svelte-getting-started)
---
+++ svelte-il-pomodoro/src/TaskList.svelte
@@ -0,0 +1,19 @@
+<script>
+  const tasks = [
+    "plan some fun trip with Teo",
+    "buy some flowers to my wife",
+    "write an article about Svelte"
+  ];
+</script>
+
+<ul>
+  {#each tasks as task}
+    <li>{task}</li>
+  {/each}
+</ul>
+
+<style>
+  ul {
+    list-style: none;
+  }
+</style>
